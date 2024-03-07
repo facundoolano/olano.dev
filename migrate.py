e9fdd5833ec7af46fff47cf08fe33574e30debc5
facundoolano <facundo.olano@gmail.com>
2024-03-07 18:19:04 -0300

    update migrate to handle dirs

--- migrate.py
+++ migrate.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 import os
+import re
 import sys
 
 
 def migrate_dir(src):
-    pass
+    posts = {}
+    for filename in os.listdir(src):
+        if re.match(r'^\d{4}-\d{2}-\d{2}', filename):
+            dest = "-".join(filename.split("-")[3:])
+            posts[filename] = dest
+            print(filename, "->", dest)
+
+    os.makedirs("blog", exist_ok=True)
+    for filename, dst in posts.items():
+        content = migrate_file(os.path.join(src, filename))
+        dst_path = os.path.join("blog", dst)
+        for filename, dst in posts.items():
+            content = content.replace("file:../" + filename,
+                                      "file:" + dst)
+
+        with open(dst_path, "w") as file:
+            file.write(content)
+            print("wrote", dst_path)
 
 
 def migrate_file(src):
